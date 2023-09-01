6ccd84a40121cb74d9c7a7e90200a63122530b28
Petrowykh <a.petrowykh@gmail.com>
2023-09-01 16:23:04 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,116 @@
+# sps - system of parsing (Sosedi)
+
+# version 0.1
+
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import myparser as mp
+import myutils as mu
+
+import time
+
+
+
+def info():
+    infoprice = mp.ParserInfo()
+    link = 'https://infoprice.by/?search=4810112010516'
+    st.text(link)
+    st.title('Вода минеральная "Darida" лечебно-столовая газ. 1.5л РБ')
+    rrr = infoprice.get_price(link)
+    st.table(rrr)
+    
+    
+
+def reports():
+    parse_bar = st.progress(0, 'Начинаем парсинг')
+    array_price = []
+    sku = mu.load_sku('excel/new_sku.xlsx').fillna('')
+    #st.table(sku)
+    part_of_parse = 50/sku.shape[0]
+    st.sidebar.title('Отчеты')
+    st.sidebar.selectbox(
+        'Выберите отчет',
+        ('TOP-100', 'Цена 1000')
+    )
+    st.sidebar.multiselect('Включать данные',
+                   ['ГИППО', 'Евроторг', 'Санта'])
+    sku['gippo_price'] = 0
+    sku['eurotorg_price'] = 0
+    if st.sidebar.button('Сформировать'):
+        gippo = mp.ParserGippo()
+        
+        links = sku['gippo']
+        
+        for count, link in enumerate(links):
+            parse_bar.progress(int(count*part_of_parse), 'Гиппо')
+            if link != '':
+                array_price.append(gippo.get_price(gippo.get_page(link)))
+            else:
+                array_price.append(0)
+        sku['gippo_price'] = array_price
+        sku['gippo_price'] = sku['gippo_price'].apply(lambda x: round(x, 2))
+
+        eurotorg = mp.ParserEurotorg()
+        links = sku['eurotorg']
+        array_price = []
+        first_step = True
+        for count, link in enumerate(links):
+            parse_bar.progress(int(50+count*part_of_parse), 'Евроторг')
+            if link != '':
+                res = eurotorg.get_price(link, first_step)
+                
+                first_step = False
+                array_price.append(res)
+            else:
+                array_price.append(0)
+        sku['eurotorg_price'] = array_price
+        sku['eurotorg_price'] = sku['eurotorg_price'].apply(lambda x: round(x, 2))
+
+        #st.table(sku[['code', 'name', 'gippo_price', 'eurotorg_price']])
+        ready_excel = sku[['code', 'name', 'gippo_price', 'eurotorg_price']]
+        ready_excel.to_excel('ready.xlsx')
+        parse_bar.progress(int(100), 'Готово!')
+        mu.send_letter('an-26@inbox.ru', 'ready.xlsx')
+        st.success('Все хорошо')
+
+
+
+            
+    
+
+def settings():
+    pass
+
+menu_dict = {
+    "Информация" : info,
+    "Отчеты": reports,
+    "Настройки": settings,
+}
+
+def main():
+    
+
+    st.set_page_config(
+        page_title="Sosedi Parsing System",
+        page_icon="🧊",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        
+        )
+    col_img, col_header = st.columns([1,2])
+    col_img.image('img/logo.png',width=200)
+    col_header.header('Система парсинга данных')
+    select_menu = option_menu(None, ["Информация", "Отчеты", "Настройки"], 
+        icons=["info-square", 'bar-chart-fill', 'gear'], 
+        menu_icon="cast", default_index=0, orientation="horizontal",
+        styles={
+            "nav-link-selected": {"background-color": "#314264"}
+        })
+    if select_menu in menu_dict.keys():   
+        menu_dict[select_menu]()
+
+
+
+if __name__=='__main__':
+    main()
---
+++ myparser.py
@@ -0,0 +1,85 @@
+import bs4
+import requests
+
+import logging, urllib3, time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions, Chrome
+
+logging.basicConfig(filename="parse.log", level=logging.INFO, filemode='w')
+urllib3.disable_warnings()
+
+class Parse:
+
+    def __init__(self):
+        # init parser
+        self.session = requests.Session()
+        self.session.headers = {'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36'}
+
+    def get_page(self, page_url):
+        # text of page
+        try:
+            r = self.session.get(page_url, verify=False)
+            r.encoding = 'utf-8'
+            html_page = r.text
+        except Exception as E:
+            html_page = ""
+            logging.exception(E)
+        return html_page
+    
+class ParserGippo(Parse):
+
+    def get_price(self, html):
+
+        soup = bs4.BeautifulSoup(html, 'lxml')
+        price = soup.find('div', class_="price").text.split(' ')[0]
+        try:
+            price = float(price)
+        except:
+            price = 0
+        return price
+    
+class ParserEurotorg:
+
+    def __init__(self) -> None:
+        self.options = ChromeOptions()
+        self.options.add_argument('headless')
+        self.driver = Chrome(options=self.options)
+    
+    def get_price(self, html, flag):
+        #TO DO may be add first enter to __init__
+        self.driver.get(html)
+        #time.sleep(1)
+        if flag:
+            time.sleep(1)
+            self.driver.find_element(By.XPATH, '//span[text()="Принять"]').click()
+        price = self.driver.find_element(By.CSS_SELECTOR, 'span.price_main__5jwcE').text.split(' ')[0].replace(',','.')
+        try:
+            price = float(price)
+        except:
+            price = 0
+        return price
+        
+class ParserInfo:
+    def __init__(self) -> None:
+        # self.options = ChromeOptions()
+        # self.options.add_argument('headless')
+        # self.driver = Chrome(options=self.options)
+        self.driver = Chrome()
+        
+
+    def get_price(self, html):
+
+        self.driver.get(html)
+        time.sleep(3)
+        list_price = []
+        list_net = []
+        nets = self.driver.find_elements(By.XPATH, "//div[@class='logo']//img")[1:]
+        prices = self.driver.find_elements(By.CSS_SELECTOR, 'div.price-volume')
+        for net, price in zip(nets, prices):
+             
+            list_price.append(price.text)
+            list_net.append(net.get_attribute('alt'))
+        #print(price)
+        return dict(zip(list_net, list_price))
